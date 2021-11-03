4468a1b37907b61bdc7b7a3bd733a4202668eadb
Wayne Dovey <wdovey@gmail.com>
2021-11-03 14:05:43 +1100

    adding healthcheck p3

--- app.js
+++ app.js
@@ -52,35 +52,6 @@ app.use(function(err, req, res, next) {
 // app.js: register the route. In our case, we don't want authorization for this route
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 
-const router = express.Router({});
-router.get('/', async (_req, res, _next) => {
-	// optional: add further things to check (e.g. connecting to dababase)
-	const healthcheck = {
-		uptime: process.uptime(),
-		message: 'OK',
-		timestamp: Date.now()
-	};
-	try {
-		res.send(healthcheck);
-	} catch (e) {
-		healthcheck.message = e;
-		res.status(503).send();
-	}
-});
-// export router with all routes included
-module.exports = router;
-
-// healthcheck.spec.js (services like Pingdom or Freshping do a similar approach to check whether your server is healthy)
-describe('Healthcheck', () => {
-
-	it('returns 200 if server is healthy', async () => {
-		const res = await get(`/healthcheck`, null)
-			.expect(200);
-		expect(res.body.uptime).toBeGreaterThan(0);
-	});
-
-});
-
 Database.connect(app, function(err) {
     if (err) {
         console.log('Failed to connect to database server');
