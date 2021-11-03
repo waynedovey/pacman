390729bae512b1eb6ed3f93c6eff5e3579d59618
Wayne Dovey <wdovey@gmail.com>
2021-11-03 14:11:18 +1100

    adding healthcheck p5

--- routes/healthcheck.routes.js
+++ routes/healthcheck.routes.js
@@ -21,12 +21,12 @@ module.exports = router;
 
 
 // healthcheck.spec.js (services like Pingdom or Freshping do a similar approach to check whether your server is healthy)
-describe('Healthcheck', () => {
-
-	it('returns 200 if server is healthy', async () => {
-		const res = await get(`/healthcheck`, null)
-			.expect(200);
-		expect(res.body.uptime).toBeGreaterThan(0);
-	});
-
-});
+//describe('Healthcheck', () => {
+//
+//	it('returns 200 if server is healthy', async () => {
+//		const res = await get(`/healthcheck`, null)
+//			.expect(200);
+//		expect(res.body.uptime).toBeGreaterThan(0);
+//	});
+//
+//});
